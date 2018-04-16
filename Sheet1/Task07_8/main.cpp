8916659de1be1238b12f17b63f2e788ac535b45f
Mark <s6maspri@uni-bonn.de>
2018-04-16 21:25:23 +0200

    Task 08

--- Sheet1/Task07_8/main.cpp
+++ Sheet1/Task07_8/main.cpp
@@ -4,57 +4,67 @@
 
 using namespace std;
 
-bool is_close(long double a, long double b, long double eps){
-    return fabs(a-b) < eps;
+bool is_close(double a, double b, double eps) {
+    return fabs(a - b) < eps;
 }
 
-std::tuple<int, int, int> calc_triple(long double U,long double V,long double W){
-    // We have |x| <= sqrt(W-y^2-z^2) <= sqrt(W)
-    long int bound = sqrt(W);
-    long double x;
-    for(long int i=-bound;i <= bound;i++){
-        if (i == 0) continue;
-        x = i;
+std::tuple<int, int, int> calc_triple(double U, double V, double W) {
+    /**
+     * Note:
+     * z is positive. We know xyz > 0, thus we cant have x,y and z < 0.
+     * Because we are looking for the lexicographically minimal tuple and the equations
+     * are symmetric the two options for signs are +,+,+ and -,-,+.
+     * Furthermore we have z <= sqrt(W-y^2-z^2),
+     *                 and x^2+y^2 >= xy = V/z >= V/sqrt(W)
+     * Thus             z <= sqrt(W-y^2-z^2) <= sqrt(W-V/sqrt(W))
+     */
+    long int bound = sqrt(W - V/sqrt(W));
+
+    double z;
+    for (long int i = bound; i > 0; i--) {
+        z = i;
         /**
-         * Given x we solve
+         * Given z we solve
          * (I)  x+y+z = U
          * (II) xyz = V
-         * for z (it turns out the two solutions of the
-         * quadratic equation are z and y or vice versa):
+         * for x (it turns out the two solutions of the
+         * quadratic equation are x and y or vice versa):
          */
-        long double z1 = (U-x)/2 + sqrt(-(V/x)+(U-x)*(U-x)/4);
-        long double z2 = (U-x)/2 - sqrt(-(V/x)+(U-x)*(U-x)/4);
+        double tmp = sqrt(-(V / z) + (U - z) * (U - z) / 4);
+        double x1 = (U - z) / 2 + tmp;
+        double x2 = (U - z) / 2 - tmp;
 
         /**
          * If the solution is integer and fulfills equation
          * (III) x^2+y^2+z^2 = W,
          * we return the lexicographically smaller triple,
-         * (x,z1,z2) or (x,z2,z1).
+         * (x1,x2,z) or (x2,x1,z).
          */
-        if(is_close(z1,round(z1),1e-15L) && is_close(x*x+z1*z1+z2*z2,W,1e-15L)){
-            if(z1 < z2){
-                return std::make_tuple((int)x,(int)round(z1),(int)round(z2));
-            } else if (z2 < z1){
-                return std::make_tuple((int)x,(int)round(z2),(int)round(z1));
-            } else{
-                continue;
+        if ( is_close(x1 * x1 + x2 * x2 + z * z, W,1e-2) && is_close(x1, round(x1), 1e-6)) {
+            if (x1 < x2 && x1 != z && x2 != z) {
+                return make_tuple((int) round(x1), (int) round(x2), (int) z);
+            } else if (x2 < x1 && x1 != z && x2 != z) {
+                return make_tuple((int) round(x2), (int) round(x1), (int) z);
+            } else {
+                break;
             }
         }
     }
-    return std::make_tuple(1,1,1);
+    return make_tuple(1, 1, 1);
 }
+
 
 int main() {
     int n;
 
     cin >> n;
 
-    for(int i = 0; i < n; ++i) {
-        long int U,V,W;
-        int x,y,z;
+    for (int i = 0; i < n; ++i) {
+        long int U, V, W;
+        long int x, y, z;
         cin >> U >> V >> W;
-        std::tie(x,y,z) = calc_triple(U,V,W);
-        if(x == 1 && y == 1 && z == 1){
+        std::tie(x, y, z) = calc_triple(U, V, W);
+        if (x == 1 && y == 1 && z == 1) {
             cout << "empty set" << endl;
         } else {
             cout << x << " " << y << " " << z << endl;
