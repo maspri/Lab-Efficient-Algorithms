819c4633a9458a123116437b5bc20db61a23d862
Jan <s6jahoec@uni-bonn.de>
2018-05-06 13:51:59 +0200

    New version but sadly too slow
    need to learn c++

--- Sheet2/Task05/main.py
+++ Sheet2/Task05/main.py
@@ -9,13 +9,21 @@ while(True):
 
     # linear time preprocess input array to get intervalls and occurrances
     Dict = {}
-    Dict[numbers[0]] = [0, 0, 1]    # triple contains informations(start, end, occurrance)
+    last_number = numbers[0]
+    Dict[last_number] = [0, 0, 1]    # triple contains informations(start, end, occurrance)
+    glob_most = 1
     for i in range(1, item_cnt):
-        if numbers[i] in Dict:
+        if last_number == numbers[i]:
             Dict[numbers[i]][1] += 1
             Dict[numbers[i]][2] += 1
+            tmp = Dict[numbers[i]][2]
+            if glob_most < tmp:
+                glob_most = tmp
+
         else:
             Dict[numbers[i]] = [i, i, 1]
+            last_number = numbers[i]
+
 
     for test in range(0, queries):
         i, j = map(int, input().split())
@@ -32,26 +40,23 @@ while(True):
         # subtract the cut amount of occurrances of the left number
         most = left_number_a - (i - left_number_i)
 
-        #most = left_number_a
-
         if j < left_number_j:
            most = most - (left_number_j - j)
-
-        # check if it is necessary to calculate for the right number
+          # check if it is necessary to calculate for the right number
         elif left_number != right_number:
             right_number_a = right_number_a - (right_number_j - j)
 
             if right_number_a > most:
                 most = right_number_a
 
-            # check if the interval contains more than 2 numbers
-            while left_number_j != (right_number_i - 1):
-                right_number_i, right_number_j, right_number_a = Dict[numbers[right_number_i -1]]
-
+            # check if the interval has potential for a better number
+            # that is if the most number is smaller then the left over interval and my most isnt the golbal most
+            while (most < (right_number_i - left_number_j)) &( most < glob_most):
+                right_number_i, right_number_j, right_number_a = Dict[numbers[right_number_i - 1]]
                 # no calculation needed,
                 # because all occurances are contained between the most left and most right number
 
-                if right_number_a > left_number_a:
+                if right_number_a > most:
                     most = right_number_a
         print(most)
 
