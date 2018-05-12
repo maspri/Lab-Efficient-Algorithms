79d94b78ef6c19357264ed65c1f36cc58dc586ec
Mark <s6maspri@uni-bonn.de>
2018-05-12 12:09:58 +0200

    sheet 2 task 7 working

--- Sheet1/Task07/main.cpp
+++ Sheet1/Task07/main.cpp
@@ -4,27 +4,28 @@
 
 using namespace std;
 
-std::tuple<int, int, int> calc_triple(long int U,long int V,long int W){
+tuple<long int, long int, long int> calc_triple(long int U,long int V,long int W){
     // We have |x| <= sqrt(W-y^2-z^2) <= sqrt(W)
     long int boundX = sqrt(W);
     long int x,y,z;
     for(long int i=-boundX;i <= boundX;i++){
         x = i;
-        if(i==0 || W-x*x <= 0) continue;
+        if(i==0) continue;
         long int boundY = ceil(sqrt(W-x*x));
+
         for(long int j=-boundY;j <= boundY;j++){
             y = j;
             if(j == 0 || x == y || W-x*x-y*y <= 0) continue;
 
             z = U - x - y;
 
-            if(x*y*z==V && x*x+y*y+z*z==W){
-                return std::make_tuple(x,y,z);
+            if(z != x && z != y && x*y*z==V && x*x+y*y+z*z==W){
+                return make_tuple(x,y,z);
             }
         }
 
     }
-    return std::make_tuple(1,1,1);
+    return make_tuple(1,1,1);
 }
 
 int main() {
@@ -34,9 +35,9 @@ int main() {
 
     for(int i = 0; i < n; ++i) {
         long int U,V,W;
-        int x,y,z;
+        long int x,y,z;
         cin >> U >> V >> W;
-        std::tie(x,y,z) = calc_triple(U,V,W);
+        tie(x,y,z) = calc_triple(U,V,W);
         if(x == 1 && y == 1 && z == 1){
             cout << "empty set" << endl;
         } else {
--- Sheet2/Task07/main.cpp
+++ Sheet2/Task07/main.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <limits>
-#include <algorithm>
 
 using namespace std;
-int blergh(const vector<int> &coins,int coinIndex,int V);
 
-vector<int> lookup;
+int mincoins(const vector<int> &coins, vector<int> &lookup,int V){
 
-bool myfunction (int i,int j) { return (i>j); }
+    if(V > 0 && lookup[V] != -2){
 
-int mblergh(const vector<int> &coins,int coinStart,int V){
-    if(lookup[V] != -1){
         return lookup[V];
-    } else {
-        int num = blergh(coins,coinStart,V);
-        lookup[V] = num;
-        return num;
-    }
-}
 
-int blergh(const vector<int> &coins,int coinIndex,int V){
-    if(V > 0 && coinIndex < coins.size()){
-        //try biggest number of largest coin first
-        int n  = V / coins[coinIndex];
-        for (int j = n; j >= 0; --j) {
-            int num = mblergh(coins,coinIndex+1, V - j*coins[coinIndex]);
-            if(num >= 0){
-                return j + num;
+    } else  if(V > 0){
+
+        bool possible = false;
+        int min = numeric_limits<int>::max();
+
+        for (int i = 0; i < coins.size(); ++i) {
+            if (coins[i] <= V){
+
+                int num = mincoins(coins,lookup,V-coins[i]);
+
+                if(num < min && num != -1){
+                    possible = true;
+                    min = num;
+                }
+
             }
+
         }
-        return -1;
-
-    } else if ( V > 0 && coinIndex == coins.size()) {
-
-        return -1;
+        if(possible){
+            lookup[V] = 1 + min;
+            return 1 + min;
+        } else{
+            lookup[V] = -1;
+            return -1;
+        }
 
     } else {
 
@@ -43,16 +43,22 @@ int blergh(const vector<int> &coins,int coinIndex,int V){
     }
 }
 
-
 int main() {
 
     int n = 0;
     cin >> n;
     for(int i=0;i<n;i++){
+
         int k=0,V=0;
         cin >> k >> V;
+
         vector<int> coins(k,0);
-        lookup = vector<int>(V*V,-1);
+        vector<int> lookup(V*V+1,-2);
+        vector<int> nonzerocoins;
+        nonzerocoins.reserve(coins.size());
+
+
+
         for(int j=0;j<k;j++){
 
             int b=0,e=0;
@@ -60,23 +66,22 @@ int main() {
             coins[j] = b*b+e*e;
 
         }
-        sort(coins.begin(),coins.end(),myfunction);
-
-        vector<int> nonzerocoins;
-        nonzerocoins.reserve(coins.size());
 
         for (int l = 0; l < coins.size(); ++l) {
-            if (coins[i] > 0)
-                nonzerocoins.push_back(coins[i]);
+            if (coins[l] > 0) {
+                nonzerocoins.push_back(coins[l]);
+            }
         }
 
-        int num = blergh(nonzerocoins,0,V*V);
+
+        int num = mincoins(nonzerocoins,lookup,V*V);
+
+
         if(num == -1){
             cout << "impossible" << endl;
         } else {
             cout << num << endl;
         }
-        //lookup.clear();
     }
 
 
