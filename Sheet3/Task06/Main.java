cd45819709f2809d1e1d6e691d4ab7f9e1c5cc76
infeo <schrenk.armin@hotmail.de>
2018-06-11 18:56:05 +0200

    Improving speed of input reading.

--- Sheet3/Task06/Main.java
+++ Sheet3/Task06/Main.java
@@ -1,75 +1,56 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	public static class HeapEntry {
-
-		private int vertex;
-		private int inDegree;
-
-		public HeapEntry(int vertex, int inDegree) {
-			this.vertex = vertex;
-			this.inDegree = inDegree;
-		}
-
-		public Integer getInDegree() {
-			return inDegree;
-		}
-
-		public Integer getVertex() {
-			return vertex;
-		}
-
-	}
-
 	public static int[] computeTopologicalSortingKahn(ArrayList<ArrayList<Integer>> dependencyList, int[] inDegreeCount) {
 		int numNodes = dependencyList.size();
 		//add all nodes in a heap
-		PriorityQueue<HeapEntry> minHeap = new PriorityQueue<>(numNodes * numNodes, new Comparator<HeapEntry>() {
-			@Override
-			public int compare(HeapEntry o1, HeapEntry o2) {
-				if (o1.getInDegree() < o2.getInDegree()) {
-					return -1;
-				} else if (o1.getInDegree() > o2.getInDegree()) {
-					return 1;
-				} else {
-					if (o1.getVertex() < o2.getVertex()) {
-						return -1;
-					} else if (o1.getVertex() > o2.getVertex()) {
-						return 1;
-					}
-				}
-				return 0;
-			}
-		});
-
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>(numNodes);
 		for (int v = 0; v < numNodes; v++) {
-			minHeap.add(new HeapEntry(v, inDegreeCount[v]));
+			if (inDegreeCount[v] == 0) {
+				minHeap.add(v);
+			}
 		}
 
 		//now we are doing Kahns algorithm
 		int[] topologicalSorting = new int[numNodes];
 		for (int i = 0; i < numNodes; i++) {
-			int v = minHeap.poll().getVertex();
+			int v = minHeap.poll();
 			topologicalSorting[i] = v;
 			for (Integer w : dependencyList.get(v)) {
 				inDegreeCount[w]--;
-				minHeap.add(new HeapEntry(w, inDegreeCount[w]));
+				if (inDegreeCount[w] == 0) {
+					minHeap.add(w);
+				}
 			}
 		}
 		return topologicalSorting;
 	}
 
-	public static void main(String[] args) throws FileNotFoundException {
-		try (Scanner scanner = new Scanner(System.in)) {
-			//try (Scanner scanner = new Scanner(new File("Sheet3/Task06/test.cases"))){
-			scanner.useLocale(Locale.ENGLISH);
-			while (true) {
-				int numLemmata = scanner.nextInt(); //number of lemmata
-				int numDependencies = scanner.nextInt(); //number of dependencies
+	private static int getWhiteSpace(String s) {
+		int i = 0;
+		while (!Character.isWhitespace(s.charAt(i))) {
+			i++;
+		}
+		return i;
+	}
 
+	public static void main(String[] args) throws IOException {
+		//try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			try (BufferedReader reader = new BufferedReader(new FileReader("Sheet3/Task06/test.cases"))) {
+			while (true) {
+				String line = reader.readLine();
+				int whitespacePosition = getWhiteSpace(line);
+				int numLemmata = Integer.valueOf(line.substring(0, whitespacePosition));
+				int numDependencies = Integer.valueOf(line.substring(whitespacePosition + 1, line.length()));
+
+				if (numLemmata == 0 && numDependencies == 0) {
+					break;
+				}
+
+				//ArrayList<Integer> [] dependencyLists = new ArrayList[numLemmata];
+				//Arrays.fill(dependencyLists, new ArrayList<Integer>(numLemmata));
 				ArrayList<ArrayList<Integer>> dependencyLists = new ArrayList<>(numLemmata);
 				for (int lemma = 0; lemma < numLemmata; lemma++) {
 					dependencyLists.add(new ArrayList<Integer>(numLemmata));
@@ -77,9 +58,12 @@ public class Main {
 
 				int[] inDegreeCount = new int[numLemmata];
 				for (int i = 0; i < numDependencies; i++) {
-					int v = scanner.nextInt() - 1;
-					int w = scanner.nextInt() - 1;
+					line = reader.readLine();
+					whitespacePosition = getWhiteSpace(line);
+					int v = Integer.valueOf(line.substring(0, whitespacePosition)) - 1;
+					int w = Integer.valueOf(line.substring(whitespacePosition + 1, line.length())) - 1;
 					dependencyLists.get(v).add(w);
+					//dependencyLists[v].add(w);
 					inDegreeCount[w]++;
 				}
 
@@ -89,12 +73,7 @@ public class Main {
 					System.out.print((topologicalSorting[i] + 1) + " ");
 				}
 				System.out.print("\n");
-
-				if (scanner.nextInt() == 0 && scanner.nextInt() == 0) {
-					break;
-				}
 			}
-
 		}
 	}
 
